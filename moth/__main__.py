d37f7a5ff076b9dc7661c5e50e20bd55e9462c6a
maythe15 <alex.ilves@outlook.com>
2024-02-04 18:01:54 -0500

    moved to a new folder and replaced init with __main__

--- moth/__main__.py
+++ moth/__main__.py
@@ -2,9 +2,10 @@ if __name__=='__main__':
     import argparse
     import sqlalchemy
     import sqlalchemy_utils
-    import utils
-    import server
+    import moth.utils
+    import moth.server
     import sqlalchemy.orm as orm
+    import sys
     parser = argparse.ArgumentParser()
     subs = parser.add_subparsers(dest='command')
     make = subs.add_parser('create')
@@ -12,7 +13,7 @@ if __name__=='__main__':
     run = subs.add_parser('run')
     run.add_argument('-f', '--file', action='store', default='moth.db', help='File to store DB into')
     run.add_argument('-p', '--port', action='store', type=int, default=1000, help='Server port')
-    args = parser.parse_args()
+    args = parser.parse_args(args=None if sys.argv[1:] else ['--help'])
     if args.command == 'create':
         engine = sqlalchemy.create_engine(f"sqlite:///{args.file}")
         if not sqlalchemy_utils.database_exists(engine.url):
